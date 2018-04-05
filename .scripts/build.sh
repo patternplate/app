ab8e81174d57d6b6c89eee31b56d972626accb61
Mario Nebl <hello@mario-nebl.de>
2018-04-05 22:41:48 +0200

    chore: remove debug comments

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -2,7 +2,7 @@
 set -e
 set -u
 
-# yarn electron-webpack prod
+yarn electron-webpack prod
 
 ./.scripts/copy-module.js \
   --bin npm \
@@ -12,4 +12,4 @@ set -u
   --out local_modules \
   --ignore electron
 
-# CSC_IDENTITY_AUTO_DISCOVERY=false yarn electron-builder --dir
+CSC_IDENTITY_AUTO_DISCOVERY=false yarn electron-builder --dir
